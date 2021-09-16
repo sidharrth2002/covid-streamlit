07c9c8d82386b7f0158ba3c9139a14c7b0c3d89d
eugenekan55 <71750094+eugenekan55@users.noreply.github.com>
2021-09-16 18:41:07 +0800

    finished

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.title('An Exploration of Covid-19 in Malaysia')
+
+cases_malaysia = pd.read_csv('./data/cases_malaysia.csv')
+cases_state = pd.read_csv('./data/cases_state.csv')
+deaths_state = pd.read_csv('./data/deaths_state.csv')
+
+cases_state = cases_state.pivot(index='date', columns='state', values='cases_new')
+st.write('Correlations')
+cases_correlations = cases_state.corr(method='pearson')
+
+fig, ax = plt.subplots(figsize=(20, 20))
+st.markdown('### Correlations between Cases Among States')
+sns.heatmap(cases_correlations, annot=True, ax=ax)
+st.write(fig)
+
+#q1 :
+
+#q2 : daily new cases
+
+#q3 : what is the reasons cause that the numbers of new cases increase
+#number of import case
+#number of testing
+
+#q4 :
