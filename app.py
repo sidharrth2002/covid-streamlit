d06586cc42a1e4c1fc68630b7f5f2a5a91d2874a
Sidharrth Nagappan <sidharrth2002@gmail.com>
2021-09-20 00:23:42 +0800

    streamlit progress

--- app.py
+++ app.py
@@ -4,16 +4,55 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import altair as alt
 from altair import Chart, X, Y, Axis, SortField, OpacityValue
-from functions import get_result
+from functions import get_result, get_best_features
 import numpy as np
+import plotly.express as px
+from plotly.graph_objects import Heatmap
+from plotly.subplots import make_subplots
 
 st.title('An Exploration of Covid-19 in Malaysia')
 
 cases_malaysia = pd.read_csv('./data/cases_malaysia.csv')
 cases_malaysia.fillna(0, inplace=True)
 cases_state = pd.read_csv('./data/cases_state.csv')
+cases_state_pivoted = cases_state.pivot_table(index='date', columns='state', values='cases_new')
 cases_state.fillna(0, inplace=True)
 deaths_state = pd.read_csv('./data/deaths_state.csv')
+deaths_pivoted = deaths_state.pivot_table(index='date', columns='state', values='deaths_new')
+tests_state = pd.read_csv('./data/tests_state.csv')
+tests_pivoted = tests_state.pivot(index='date', columns='state', values='total')
+
+cases_state = pd.read_csv('./data/cases_state.csv', index_col=0)
+cases_state_pivoted = cases_state.pivot(index='date', columns='state', values='cases_new')
+deaths_state = pd.read_csv('./data/deaths_state.csv')
+deaths_state_pivoted = deaths_state.pivot(index='date', columns='state', values='deaths_new')
+tests_state = pd.read_csv('./data/tests_state.csv')
+tests_state_pivoted = tests_state.pivot(index='date', columns='state', values='total')
+quarantine_state = pd.read_csv('./data/pkrc.csv')
+quarantine_state_pivoted = quarantine_state.pivot(index='date', columns='state', values='admitted_covid')
+discharge_quarantine_state_pivoted = quarantine_state.pivot(index='date', columns='state', values='discharge_covid')
+icu_state = pd.read_csv('./data/icu.csv')
+icu_state_pivoted = icu_state.pivot(index='date', columns='state', values='icu_covid')
+hospital = pd.read_csv('./data/hospital.csv')
+hospital_admitted_pivoted = hospital.pivot(index='date', columns='state', values='admitted_covid')
+hospital_discharged_pivoted = hospital.pivot(index='date', columns='state', values='discharged_covid')
+population_state = pd.read_csv('./data/population.csv', index_col=0)
+
+states_list = ['Pahang', 'Kedah', 'Johor', 'Selangor']
+states = {}
+
+for state in states_list:
+    df = pd.DataFrame()
+    df['cases'] = cases_state_pivoted[state]
+    df['deaths'] = deaths_state_pivoted[state]
+    df['tests'] = tests_state_pivoted[state]
+    df['quarantine'] = quarantine_state_pivoted[state]
+    df['discharge_quarantine'] = discharge_quarantine_state_pivoted[state]
+    df['icu'] = icu_state_pivoted[state]
+    df['hospital_admitted'] = hospital_admitted_pivoted[state]
+    df['hospital_discharged'] = hospital_discharged_pivoted[state]
+    df.fillna(0, inplace=True)
+    states[state] = df
 
 st.write(
     '''
@@ -22,7 +61,7 @@ st.write(
     '''
 )
 
-st.markdown('## Exploratory Data Analysis')
+st.markdown('## Preprocessing')
 st.markdown(
     '''
     The following preprocessing steps were employed to gain a deeper understanding of the data.\n
@@ -32,16 +71,11 @@ st.markdown(
     '''
 )
 
+st.markdown('## Exploratory Data Analysis')
 st.write(
     '''
     The following EDA steps were performed:\n
     1. **Statistical Summary**: We used the `describe()` method to get a statistical summary of the cleaned data.\n
-    '''
-)
-st.dataframe(cases_malaysia.describe())
-
-st.write(
-    '''
     2. **Unique States**: What are the unique states in the dataset?\n
     '''
 )
@@ -108,11 +142,6 @@ st.markdown('''
     ### Outliers
     We first group the data by state and wave. Then we normalise the data by the population of the state
     to account for the different sizes. A state with an abnormally different number of cases in a wave is considered an outlier.
-    Beyond just plotting them, we will identify the risk levels:\n
-    1. Upper outliers are considered VERY HIGH RISK states.\n
-    2. States between Q3 and Q4 are considered HIGH RISK states.\n
-    3. States between Q1 and Q2 are considered MEDIUM RISK states.\n
-    4. Lastly, states below Q1 are considered LOW RISK states.\n
 ''')
 num_of_state = 16
 wave1_each_states = cases_state.iloc[:170*num_of_state]
@@ -120,10 +149,108 @@ wave2_each_states = cases_state.iloc[170*num_of_state:430*num_of_state]
 wave3_each_states = cases_state.iloc[430*num_of_state:]
 
 get_result(wave1_each_states, 1, st)
+st.markdown('''
+    It's quite clear that in the first wave, the majority of the country is in the
+    lower end. However, there are a few states that stand out as outliers, and these come as no surprise: Negeri Sembilan,
+    W.P. Kuala Lumpur and Putrajaya.
+    Note that this also accounts for their population, which is why Putrajaya ranks higher than Selangor itself.
+    Kedah, Kelantan, Perlis and Sabah are relatively low risk.
+    '''
+)
+
 get_result(wave2_each_states, 2, st)
+st.markdown('''
+    By the second wave, most of the country has caught up and there are no more outliers. There is a right skew,
+    so the majority of states are now in the upper end of the range.
+''')
+
 get_result(wave3_each_states, 3, st)
+st.markdown('''
+    In the third wave, the distribution is almost symmetric. The case number for the worst hit state has increased, as observed earlier.
+    More of the country is at a high risk level, with the exception of perhaps Perlis, which stayed consistently low across all 3 waves.
+''')
+
+st.markdown(
+'''
+    ## What states exhibit strong correlation with (i) Pahang and (ii) Johor?\n
+    We can study correlation based on case number, deaths and testing. It doesn't matter if
+    they are on different scales as long as they change in a similar pattern.
+''')
+
+cases_correlation = cases_state_pivoted.corr()
+tests_correlation = tests_pivoted.corr()
+deaths_correlation = deaths_pivoted.corr()
+
+st.markdown('### Correlation by Cases')
+cases_heatmap = px.imshow(cases_correlation)
+st.plotly_chart(cases_heatmap)
+st.write('Kedah, Terrengganu and Perak exhibit strong correlation with Pahang in daily new cases.')
+cases_correlation_pahang = cases_correlation['Pahang'].sort_values(ascending=False)
+cases_correlation_pahang = pd.DataFrame(cases_correlation_pahang)
+st.write(cases_correlation_pahang.iloc[1:4])
+st.write('''
+    Pulau Pinang, Terengganu and Perak are highly correlated with Johor in terms of new daily cases.
+''')
+cases_correlation_Johor = cases_correlation['Johor'].sort_values(ascending=False)
+cases_correlation_Johor = pd.DataFrame(cases_correlation_Johor)
+st.write(cases_correlation_Johor.iloc[1:4])
+
+st.markdown('### Correlation by Tests')
+tests_heatmap = px.imshow(tests_correlation)
+st.plotly_chart(tests_heatmap)
+st.write('Kedah, Johor and Perak exhibit strong correlation with Pahang in terms of testing per day.')
+tests_correlation_pahang = tests_correlation['Pahang'].sort_values(ascending=False)
+tests_correlation_pahang = pd.DataFrame(tests_correlation_pahang)
+st.write(tests_correlation_pahang.iloc[1:4])
+
+st.write('''
+    Kedah, Perlis and Pulau Pinang are strongly correlated with Johor.
+''')
+tests_correlation_Johor = tests_correlation['Johor'].sort_values(ascending=False)
+tests_correlation_Johor = pd.DataFrame(tests_correlation_Johor)
+st.write(tests_correlation_Johor.iloc[1:4])
+
+st.markdown('### Correlation by Deaths')
+deaths_heatmap = px.imshow(deaths_correlation)
+st.plotly_chart(deaths_heatmap)
+st.write('Negeri Sembilan, Johor and Selangor exhibit a strong correlated with Pahang in terms of deaths.')
+deaths_correlation_pahang = deaths_correlation['Pahang'].sort_values(ascending=False)
+deaths_correlation_pahang = pd.DataFrame(deaths_correlation_pahang)
+st.write(deaths_correlation_pahang.iloc[1:4])
+
+st.write('Pulau Pinang, Sabah and Perak exhibit strong correlation with Pahang in daily new deaths.')
+deaths_correlation_Johor = deaths_correlation['Johor'].sort_values(ascending=False)
+deaths_correlation_Johor = pd.DataFrame(deaths_correlation_Johor)
+st.write(deaths_correlation_Johor.iloc[1:4])
 
 
+st.markdown(
+'''
+    ## Strong features/indicators to daily cases
+    We do a feature selection of 4 features using 4 methods and then do a final vote to choose the best features for each state.
+    The 4 methods are:\n
+    1. Variance Threshold (3)\n
+    2. Mutual Info Regression\n
+    3. Chi2 Test\n
+    4. Recursive Feature Elimination (RFE)
+''')
+
+st.markdown('Pahang')
+get_best_features(states['Pahang'], st, display_scatter_plots=1)
+
+st.markdown('''
+We know that the four best features are quarantine, discharge_quarantine, icu and hospital_admitted.
+For quarantine and discharge_quarantine, we can see that there is a strong, positive and linear relationship.
+''')
+
+st.markdown('Kedah')
+get_best_features(states['Kedah'], st, display_scatter_plots=1)
+
+st.markdown('Johor')
+get_best_features(states['Johor'], st, display_scatter_plots=1)
+
+st.markdown('Selangor')
+get_best_features(states['Selangor'], st, display_scatter_plots=1)
 
 # This global variable 'bar_plot' will be used later on
 
--- functions.py
+++ functions.py
@@ -3,6 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import altair as alt
 import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.feature_selection import SelectKBest, chi2, mutual_info_regression, RFE, VarianceThreshold
+from sklearn.linear_model import LinearRegression
+from plotly.subplots import make_subplots
+import sklearn
 
 def get_result(wave,wave_num,st) :
     population = pd.read_csv('./data/population.csv')
@@ -44,10 +50,9 @@ def get_result(wave,wave_num,st) :
     
     #display the boxplot of each state total_cases / Population
 
-    fig, ax = plt.subplots(figsize=(10, 2))
-    sns.boxplot(x=outlierDetection['total_cases / Population'], ax=ax)
-    ax.set_title('Wave '+ str(wave_num))
-    st.pyplot(fig)
+    print(outlierDetection)
+    fig = px.box(outlierDetection.rename(columns={'total_cases / Population': 'Adjusted Case Number'}), x="Adjusted Case Number", title='Wave '+ str(wave_num))
+    st.plotly_chart(fig)
 
     #to calculate the IQR so we can find for the outlier
     Q1 = outlierDetection.quantile(0.25)
@@ -73,6 +78,49 @@ def get_result(wave,wave_num,st) :
 
     LowRisk = outlierDetection[outlierDetection['total_cases / Population'] < Q1['total_cases / Population']]
     print('Low Risk States')
-    
 
-    
+#the function we use to find the best features
+def get_best_features(df, st, display_scatter_plots = 0):
+    X = df.drop(['cases'], axis=1)
+    y = df['cases']
+
+    selector = VarianceThreshold(3)
+    selector.fit(df)
+    variance_best = df.columns[selector.get_support()]
+
+    selector = SelectKBest(mutual_info_regression, k=4)
+    selector.fit(X, y)
+    mutual_info_best = X.columns[selector.get_support()]
+
+    selector = SelectKBest(chi2, k=4)
+    selector.fit(X, y)
+    chi2_best = X.columns[selector.get_support()]
+
+    rfe_selector = RFE(LinearRegression(), n_features_to_select=4)
+    rfe_selector.fit(X, y)
+    rfe_best = X.columns[rfe_selector.get_support()]
+
+    columns = df.columns
+    columns_count = {}
+    for column in columns:
+        columns_count[column] = list(variance_best).count(column) + list(mutual_info_best).count(column) + list(chi2_best).count(column) + list(rfe_best).count(column)
+
+    best_features = sorted(columns_count, key=columns_count.get)[-4:]
+    grid_pos = [[1, 1], [1, 2], [2, 1], [2, 2]]
+
+    if display_scatter_plots == 1:
+        print("The best four features")
+        print("")
+        feature_subplots = make_subplots(rows=2, cols=2, subplot_titles=best_features)
+        for num in range(len(best_features)):
+            sns.scatterplot(x= best_features[num], y="cases", data=df)
+            z = np.polyfit(df[best_features[num]], df['cases'], 1)
+            p = np.poly1d(z)
+            feature_subplots.add_trace(go.Scatter(x=df[best_features[num]], y=df['cases'], mode='markers', name=best_features[num], line=go.scatter.Line()), row=grid_pos[num][0], col=grid_pos[num][1])
+
+    if 'tests' not in best_features:
+        best_features.append('tests')
+
+    st.plotly_chart(feature_subplots)
+
+    return best_features
